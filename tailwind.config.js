8dfa70946281050c6f7095bd679009e8e025bfee
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-10-27 07:32:43 +0700

    add content and theme conf in tailwind config js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      'white': "#fff", 
+      'black': '#000',
+      'grey': '#686868',
+      'light-grey': '#F5F5F5',
+      'blue1': '#007EDA',
+      'blue2': '#0162A8',
+      'blue3': '#DBF0FF',
+      'yellow1': '#F7BD4A',
+      'yellow2': '#B27E15',
+      'space1': '#111827',
+      'space2': '#4b5563',
+      'horizon-text1': '#f97316',
+      'horizon-text2': '#fde047'
+    },
+    fontFamily: {
+      'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+    },
   },
   plugins: [],
 }
