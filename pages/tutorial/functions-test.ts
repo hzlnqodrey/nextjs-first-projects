e0dc9c0512d49bb4f6a953b507749b8d83204ba8
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-14 23:47:01 +0700

    NO BS TS: #idk - // union data type

--- pages/tutorial/functions-test.ts
+++ pages/tutorial/functions-test.ts
@@ -2,3 +2,8 @@ import multiply, { addStrings } from './functions'
 
 console.log(multiply(202, 30));
 console.log(multiply(100, 500));
+
+console.log(addStrings("a", "b"))
+console.log(addStrings("a", 2))
+console.log(addStrings("a"))
+
--- pages/tutorial/functions.ts
+++ pages/tutorial/functions.ts
@@ -1,8 +1,12 @@
+// function
 function multiply(a: number, b: number): number {
     return a * b
 }
 
 export default multiply;
 
+// export const
 export const addStrings = (str1: string, str2: string): string => `${str1} ${str2}`
 
+// union data type
+export const format = (title: string, param: string | number): string => `${title} ${param}`
