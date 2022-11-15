bcdb97caf48a96e85c9fc4d2ac43d7542b1b91fe
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-15 21:26:10 +0700

    implement simple like button

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,31 @@
+'use client';
+
 export default function Page() {
 
   const names: string[] = ["Rivano ATK", "Hazlan Muhammad Qodri", "Riko", "Fauzan"]
 
+  function handleClick(): void {
+    console.log('Increment like count');
+    
+  }
+
   return (
     <div className="container mx-auto px-20 py-10 border-1 border-red">
       <main className="flex flex-col justify-center align-bottom text-green-400 border-2 border-sky-500">
+        {/* Title */}
         <h1 className="text-3xl font-bold underline text-center">
           Hello, Next.js!
         </h1>
       
+        {/* List */}
         <ul className="list-none border-4  border-red-500 text-center mt-5">
           {names.map((name) => (
             <li key={name}>{name}</li>
           ))}
         </ul>
+
+        {/* Button */}
+        <button onClick={handleClick}> Like </button>
       </main>
     </div>
   )
