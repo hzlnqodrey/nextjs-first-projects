2c6355c8410d78ac01bacb82ad5a8ffea99a59fe
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-11-15 21:21:33 +0700

    idk what happen, but nextjs and tailwind work

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+export default function Page() {
 
-export default function Home() {
-
-  const names: string[] = ["Hazlan Qodrey", "Rivano ATK", "Fauzan", "Cemilik"]
+  const names: string[] = ["Rivano ATK", "Hazlan Muhammad Qodri", "Riko", "Fauzan"]
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
+    <div className="container mx-auto px-20 py-10 border-1 border-red">
+      <main className="flex flex-col justify-center align-bottom text-green-400 border-2 border-sky-500">
+        <h1 className="text-3xl font-bold underline text-center">
+          Hello, Next.js!
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-
-        {/* TUTORIAL LEARNING */}
-        {/* 1. Props */}
-        <div className='container mx-auto px-10 py-10 '>
-          <ul className='list-none'>
-            {names.map((name) => (
-              <li key={name}>{name}</li>
-            ))}
-          </ul>
-        </div>
-
-        {/* 2. State */}
-        
+      
+        <ul className="list-none border-4  border-red-500 text-center mt-5">
+          {names.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
